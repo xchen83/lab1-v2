4a8488f2a2b32fc3b7510f5941508085725d1392
Xuan Chen <xchen83@uw.edu>
2024-01-04 15:18:29 -0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:;
+}
+
+void loop() {
+  Serial.println("Hello RP2040!");
+  delay(2000);
+}
